6f2fa0a2f6e157e18be924b5a599b4753968279c
Srihariharan B V <sriharib@cloudassert.com>
2020-03-30 10:36:12 +0530

    Add project files.

---
+++ Guacamole/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Guacamole.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
